0564c9750e481c9088cbc3984b2b9d17d46444af
Simon Diehl <j.simon.diehl@googlemail.com>
2018-03-18 21:35:23 +0100

    Added skeleton for BMS state machine

--- Schaltungsentwicklung_DevBoard.X/BMS.c
+++ Schaltungsentwicklung_DevBoard.X/BMS.c
@@ -1,15 +1,62 @@
+// TODO
+// * Regelung für Ladestrom und -spannung
+// * FSM für BMS bauen mit Logik, wann die Zelle getrennt wird
+
 #include "BMS.h"
 
 #include <mcc_generated_files/adc1.h>
 
-Measurements_t measurements = {0};
+BMS_Configuration_t BMS_configuration;
 
-void BMS_calculate_measurements (void)
+void BMS_init (void)
 {
+    // Nothing to do here yet
+    return;
+}
+
+void BMS_step (void)
+{
+    BMS_State_t current_state = discharging;
+    static BMS_State_t next_state = discharging;
+
+    Measurements_t measurements;
+
+    bool enable_charging = false;
+
+    // Calculate sensor values
     // Ubat = Umeas * Gain = Umeas * 1.8
     measurements.battery_voltage_mV = ( (uint32_t)(ADC1_measured_voltage_mV[2]) * 18) / 10;
     // Ibat = Umeas * Gain + Offset = Umeas * 0.8772 mA/mV - 600 mA
     measurements.battery_current_mA = ((int32_t)(ADC1_measured_voltage_mV[0]) * 8772) / 10000 - 600;
     // Tbat = Umeas * Gain + Offset = Umeas * 0.04564 K/mV - 50 °C
     measurements.battery_temperature_dgC = ((int32_t)(ADC1_measured_voltage_mV[1]) * 4564) / 100000 - 50;
+
+    // BMS FSM
+    // Update current state
+    current_state = next_state;
+
+    switch (current_state) {
+        case charging: {
+
+        } break;
+        case charged: {
+
+        } break;
+        case discharging: {
+
+        } break;
+        case discharged: {
+
+        } break;
+    }
+
+    // Funktion implementieren
+    // voltage limit und current limit sind abhängig von BMS Konfiguration und derating wegen Temperatur
+    // control_charging(enable_charging, voltage_limit, current_limit);
+
+
+    // Check sensor values against defined limits
+    // voltage_limits_violated = measurements.battery_voltage_mV < BMS_configuration.min_battery_voltage_mV || measurements.battery_voltage_mV > BMS_configuration.max_battery_voltage_mV;
+    // current_limits_violated = measurements.battery_current_mA < BMS_configuration.min_battery_current_mA || measurements.battery_current_mA > BMS_configuration.max_battery_current_mA;
+    // temperature_limits_violated = measurements.battery_temperature_dgC < BMS_configuration.min_battery_temperature_dgC || measurements.battery_temperature_dgC > BMS_configuration.max_battery_temperature_dgC;
 }
--- Schaltungsentwicklung_DevBoard.X/BMS.h
+++ Schaltungsentwicklung_DevBoard.X/BMS.h
@@ -10,7 +10,32 @@ typedef struct {
     int8_t battery_temperature_dgC;
 } Measurements_t;
 
-//! Calculate measured values
+typedef struct {
+    uint16_t min_battery_voltage_mV;
+    uint16_t max_battery_voltage_mV;
+    uint16_t max_battery_charge_current_mA;
+    uint16_t max_battery_discharge_current_mA;
+    int8_t min_battery_temperature_dgC;
+    uint8_t max_battery_temperature_dgC;
+} BMS_Configuration_t;
+
+typedef enum {
+    charging,
+    charged,
+    discharging,
+    discharged
+} BMS_State_t;
+
+extern BMS_Configuration_t BMS_configuration;
+
+//! Initialise BMS module
 //!
-//! Do oversampling and conversion to useful values by evaluating the ADC measurements.
-void BMS_calculate_measurements (void);
+//! \pre
+//!     BMS_configuration is set
+void BMS_init (void);
+
+//! Perform one BMS cycle
+//!
+//! Must be executed with 1 ms period
+//! Check sensor values against limits and act accordingly
+void BMS_step (void);
