aa42ec7d45aedc3366823a1db7223084b077dcca
Simon Diehl <j.simon.diehl@googlemail.com>
2018-03-29 15:16:10 +0200

    Minor changes

--- Schaltungsentwicklung_DevBoard.X/BMS.c
+++ Schaltungsentwicklung_DevBoard.X/BMS.c
@@ -40,15 +40,15 @@ typedef enum {
 BMS_State_t current_state;
 
 void BMS_init (void)
-{    
+{
     current_state = Init;
-    
+
     U_SENS_DISABLE_SetLow();
-    
+
     delay_ms(5);
-    
+
     update_measurements();
-    
+
     measurements.SOC_promille = get_soc_by_ocv(measurements.battery_voltage_mV);
     measurements.current_accumulator_mA = measurements.SOC_promille * (BMS_configuration.cell_capacity_mAs / 50);
 }
@@ -56,12 +56,12 @@ void BMS_init (void)
 void BMS_step (void)
 {
     static BMS_State_t next_state = Init;
-    
+
     static Tick_t fault_timer_ms;
 
     bool enable_charging = false;
     bool enable_led = false;
-    
+
     update_measurements();
 
     // BMS FSM
@@ -104,7 +104,7 @@ void BMS_step (void)
             } else {
                 next_state = Charging;
             }
-            
+
             enable_led = true;
             enable_charging = true;
         } break;
@@ -117,7 +117,7 @@ void BMS_step (void)
             } else {
                 next_state = Charged;
             }
-            
+
             enable_charging = false;
             enable_led = true;
         } break;
@@ -130,27 +130,27 @@ void BMS_step (void)
             } else {
                 next_state = Discharging;
             }
-            
+
             enable_led = true;
         } break;
         case Discharged: {
             GPIO_8_SetHigh();
             delay_ms(5000);
             BAT_DISCONNECT_SetHigh();
-        } break;        
+        } break;
         case Fault: {
             Error_Handler();
         }
         default: Error_Handler();
     }
-    
+
     control_led (enable_led);
     control_charger (enable_charging);
-    
+
     // Prints
     {
         static Tick_t last_tick_ms = 0;
-        
+
         if (tick_ms - last_tick_ms >= 1000) {
             printf("Current State = %s");
             printf("Measured Values:\n");
@@ -160,15 +160,15 @@ void BMS_step (void)
             printf("Accumulated_Current = %d mA\n", measurements.current_accumulator_mA);
             printf("SOC = %d permille\n", measurements.SOC_promille);
             printf("Remaining time = %d\n", current_state == Discharging ? measurements.remaining_discharge_time_min : measurements.remaining_charge_time_min);
-            
+
             if (current_state == Discharging) {
                 printf("Remaining time = %d\n", measurements.remaining_discharge_time_min);
             } else if (current_state == Charging) {
                 printf("Remaining time = %d\n", measurements.remaining_charge_time_min);
             }
-            
+
             printf("\n");
-            
+
             last_tick_ms = tick_ms;
         }
     }
@@ -245,33 +245,33 @@ static void update_measurements (void)
     // Offset = 0
     measurements.EXT_ADC_1_mV = (oversampled_values.EXT_AD_1 * REFERENCE_VOLTAGE_mV) >> 11;
     measurements.EXT_ADC_2_mV = (oversampled_values.EXT_AD_2 * REFERENCE_VOLTAGE_mV) >> 11;
-    
+
     // Accumulate current every 50 ms
     {
         static Tick_t last_tick_ms = 0;
-        
-        if (tick_ms - last_tick_ms >= 50) {            
+
+        if (tick_ms - last_tick_ms >= 50) {
             // Limit underflow to zero
             if (-measurements.battery_current_mA > (int32_t)measurements.current_accumulator_mA) {
                 measurements.current_accumulator_mA = 0;
             } else {
                 measurements.current_accumulator_mA += measurements.battery_current_mA;
             }
-            
+
             last_tick_ms = tick_ms;
         }
     }
-    
+
     // Calculate SOC and remaining charge every 1 s
     {
         static Tick_t last_tick_ms = 0;
         static int16_t battery_current_delayed_mA = 0;
-        
+
         if (tick_ms - last_tick_ms >= 1000) {
             // Calculate SOC based on accumulated current and cell capacity
             // @todo add formula
             measurements.SOC_promille = ( measurements.current_accumulator_mA ) / ( BMS_configuration.cell_capacity_mAs / 50 );
-            
+
             // Filter Battery Current with PT1 for smoother estimate of remaining discharge time.
             //
             // T = 1 s
@@ -279,13 +279,13 @@ static void update_measurements (void)
             // y * (1 - 0.3679/z) = u * 0.6321/z -> y = (u * 0.6321)
             measurements.battery_current_filtered_mA = (battery_current_delayed_mA * 6321 + measurements.battery_current_filtered_mA * 3679) / 10000;
             battery_current_delayed_mA = measurements.battery_current_mA;
-            
+
             // Estimate remaining charge / discharge time based on SOC and battery current
             // The calculation is different for charging and discharging:
             // * Linear extrapolation is used to estimate the remaining discharge time
-            // * CC / CV aware extrapolation is used to estimate the remaining charge time
+            // * CC / CV aware extrapolation is used to estimate the remaining charge time (@todo Ist noch nicht drin, bis jetzt lineare extrapolation fÃ¼r beides)
             // @todo
-            // So anpassen, dass automatisch positive und negative ströme unterschieden werden.
+            // So anpassen, dass automatisch positive und negative strï¿½me unterschieden werden.
             // Nicht mit Vorzeichenumkehr, sondern Rechnung mit signed integer und dann anhand des Vorzeichens entscheiden.
             switch (current_state) {
                 case Discharging: {
@@ -306,7 +306,7 @@ static void update_measurements (void)
                 } break;
                 // @todo what should be done in other cases?
             }
-            
+
             last_tick_ms = tick_ms;
         }
     }
@@ -378,10 +378,10 @@ static void control_led (bool enable_led)
 {
     static uint16_t pwm_value = 0;
     static bool wraparound = false;
-    
+
     if (GPIO_9_GetValue() && !wraparound) {
         pwm_value += pwm_value / 16 + 10;
-        
+
         if (pwm_value >= 40000) {
             pwm_value = 0;
             wraparound = true;
@@ -389,7 +389,7 @@ static void control_led (bool enable_led)
     } else if (!GPIO_9_GetValue() ) {
         wraparound = false;
     }
-    
+
     if (enable_led) {
         OC5_PWMPulseWidthSet(pwm_value);
     } else {
@@ -400,30 +400,29 @@ static void control_led (bool enable_led)
 static void control_charger (bool enable_charging)
 {
     bool hyst_controller_enabled;
-    
+
     if (BMS_configuration.end_of_charge_voltage_mV < 4150) {
         hyst_controller_enabled = true;
     } else {
-        
         hyst_controller_enabled = false;
     }
-    
+
     if (enable_charging) {
         if (hyst_controller_enabled) {
             static bool charging = false;
-            
-            if (charging) {                
+
+            if (charging) {
                 if (measurements.battery_voltage_mV >= BMS_configuration.end_of_charge_voltage_mV) {
                     charging = false;
                 }
-                
+
                 CHG_DISABLE_SetLow();
                 GPIO_6_SetHigh();
             } else {
                 if (measurements.battery_voltage_mV < BMS_configuration.end_of_charge_voltage_mV - 20) {
                     charging = true;
                 }
-                
+
                 CHG_DISABLE_SetHigh();
                 GPIO_6_SetLow();
             }
